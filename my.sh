b1690c03c3ced8ab954f11bb9b478977ed487831
DongLi Si <kgdboc@gmail.com>
2020-04-27 09:55:15 +0800

    u

--- my.sh
+++ my.sh
@@ -1,32 +1,36 @@
 alias c=cat
+alias f=find
 alias d=date
-alias n=ncdu
 alias du="du -sh"
-alias ua="uname -a"
 alias s=systemctl
 alias fh="free -h"
-alias top="top -c"
-alias ia="ip a"
-alias ir="ip route"
-alias st="ss -nltp"
-alias su="ss -nlup"
-alias sl="ss -nltup"
 alias cx="chmod +x"
 alias r="readlink -f"
 alias p="ps -ef | grep"
-alias f="find . | grep"
-alias m="MANWIDTH=165 man"
-alias cip="curl https://ipinfo.io/ip"
-
 alias em="emacsclient -c -F \"'(fullscreen . maximized)\" -a=\"\""
-alias eq="emacs -nw -q --eval \"(setq make-backup-files nil)\""
 
-alias h="history | grep"
+alias rqa="rpm -qa"
+alias rqf="rpm -qf"
+alias rql="rpm -ql"
+
+alias ua="uname -a"
+alias ur="uname -r"
+
+alias ia="ip a"
+alias ir="ip route"
+
+alias st="ss -nltp"
+alias su="ss -nlup"
+alias sl="ss -nltup"
+
+alias hg="history | grep"
 alias hr="history -r"
 alias hw="history -w"
 
 alias pk="pkill -9"
-alias k9="kill -9"
+alias k2="kill -s 2"
+alias k6="kill -s 6"
+alias k9="kill -s 9"
 
 alias ll="ls -l"
 alias ls="ls -F"
@@ -41,7 +45,15 @@ alias gl="git log"
 alias gacp="git add . && git commit -m u &> /dev/null && git push"
 
 alias dy="dnf install -y --nogpgchec"
-alias pdy="proxychains4 -q dnf install -y --nogpgchec"
 alias ds="dnf search"
 alias dp="dnf provides"
+alias pdy="proxychains4 -q dnf install -y --nogpgchec"
 
+if [ "$INSIDE_EMACS" ]
+then
+  :
+elif [ -n "$BASH_VERSION" ] && [ -f /usr/share/fzf/shell/key-bindings.bash ]; then
+  . /usr/share/fzf/shell/key-bindings.bash
+else
+  :
+fi
