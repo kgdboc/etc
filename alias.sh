210910b4bd3e8ff483b112cdac9e6a12f6e23f63
Dongli Si <kgdboc@gmail.com>
2021-01-16 22:39:38 +0800

    u

--- alias.sh
+++ alias.sh
@@ -1,15 +1,14 @@
-alias c=cat
-alias f=find
 alias d=date
+alias dd="dd status=progress"
 alias du="du -sh"
-alias s=systemctl
-alias gip="geoiplookup -l"
+alias df="df -hT"
 alias fh="free -h"
 alias cx="chmod +x"
 alias r="readlink -f"
-alias wl="wc -l"
-alias p="ps -ef | grep"
 alias em="emacsclient -c -F \"'(fullscreen . maximized)\" -a=\"\""
+
+alias pq="ps -aux | grep qemu"
+alias sq="sudo qemu-wrapper"
 
 alias rqa="rpm -qa"
 alias rqf="rpm -qf"
@@ -25,38 +24,18 @@ alias sst="ss -nltp"
 alias ssu="ss -nlup"
 alias ssl="ss -nltup"
 
-alias hg="history | grep"
 alias hr="history -r"
 alias hw="history -w"
 
-alias pk="pkill -9"
-alias k2="kill -s 2"
-alias k6="kill -s 6"
-alias k9="kill -s 9"
-
 alias ll="ls -l"
-alias ls="ls -F"
-alias la="ls -aF"
 
 alias pc="proxychains4 -q"
 alias pb="proxychains4 -q bash"
 
 alias gs="git status"
 alias gd="git diff"
-alias gl="git log"
-alias gac="git add . && git commit -m u &> /dev/null"
-alias gacp="git add . && git commit -m u &> /dev/null && git push"
 
-alias dy="dnf install -y --nogpgchec"
+alias dy="dnf install -y --nogpgcheck"
 alias ds="dnf search"
 alias dp="dnf provides"
 alias pdy="proxychains4 -q dnf install -y --nogpgchec"
-
-if [ "$INSIDE_EMACS" ]
-then
-  :
-elif [ -n "$BASH_VERSION" ] && [ -f /usr/share/fzf/shell/key-bindings.bash ]; then
-  . /usr/share/fzf/shell/key-bindings.bash
-else
-  :
-fi
